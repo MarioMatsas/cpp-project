fb8be144758f93ad8b26ac44cd6a11c50a4a4677
Basil Papadimas <basil@excession.gr>
2024-01-29 17:57:35 +0200

    how did that get there

--- assets/GameObject.cpp
+++
@@ -1,9 +0,0 @@
-#include "GameObject.h"
-
-int GameObject::m_next_id = 1;
-
-GameObject::GameObject(const std::string& name, const std::string& classType)
-	: m_name(name), m_id(m_next_id++), m_state(GameState::getInstance()), m_class(classType)
-{
-
-}
