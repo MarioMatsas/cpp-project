2146e70619f32f6e0aac25529e650193a458017c
Basil Papadimas <basil@excession.gr>
2024-02-03 22:25:13 +0200

    ok

--- src/Level.cpp
+++ src/Level.cpp
@@ -70,6 +70,7 @@ void Level::draw()
         br.texture = std::string(ASSET_PATH) + "GameOver.png";
         graphics::drawRect(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, WINDOW_WIDTH, WINDOW_HEIGHT, br);
         graphics::playSound(std::string(ASSET_PATH) + std::string("player_damage_sound.wav"), 0.0f, false); // hack
+        graphics::stopMusic();
         return;
     }
     graphics::drawRect(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, WINDOW_WIDTH,
